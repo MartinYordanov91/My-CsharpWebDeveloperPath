4360f93300cce700635e829b5c2234e8e648653d
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-07-16 10:37:46 +0300

    Create Stack Sum.cs

---
+++ 01. Csharp Advaced Part-Advanced/01. Stacks and Queues - Lab/2. Stack Sum/Stack Sum.cs
@@ -0,0 +1,62 @@
+namespace _2._Stack_Sum
+{
+    using System;
+    using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Stack<int> stack = new();
+            foreach (int i in list)
+            {
+                stack.Push(i);
+            }
+
+            string curentInput = string.Empty;
+            while ((curentInput = ToLLower(Console.ReadLine())) != "end")
+            {
+                string[] comandARG = curentInput.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string comand = comandARG[0];
+                if(comand == "add")
+                {
+                    int intFirst = int.Parse(comandARG[1]);
+                    int intSecond = int.Parse(comandARG[2]);
+                    stack.Push(intFirst);
+                    stack.Push(intSecond);
+                }
+                else if(comand == "remove")
+                {
+                    int nNumbers = int.Parse(comandARG[1]);
+                    if(stack.Count > nNumbers)
+                    {
+                        for (int i = 0; i < nNumbers; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+
+        public static string ToLLower(string curentInput)
+        {
+            char[] chars = curentInput.ToCharArray();
+            StringBuilder sb = new();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                
+                sb.Append(char.ToLower(chars[i]));
+            }
+            return sb.ToString();
+        }
+    }
+}
