049935e458f37223e754892393d781354ee389f6
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-03 19:12:40 +0300

    Word - count

---
+++ 01. Csharp Advaced Part-Advanced/07. Streams, Files and Directories - Lab/03. WordCount/WordCount.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+        public static void CalculateWordCounts(string wordsFilePath, string
+       textFilePath, string outputFilePath)
+        {
+            Dictionary<string, int> wordsCount = new();
+            using StreamReader readerWords = new StreamReader(wordsFilePath);
+            while (!readerWords.EndOfStream)
+            {
+                string[] words = readerWords.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string word in words)
+                {
+                    if (wordsCount.ContainsKey(word.Trim()) == false)
+                    {
+                        wordsCount[word.Trim()] = 0;
+                    }
+                }
+            }
+
+            using StreamReader text = new StreamReader(textFilePath);
+            while (!text.EndOfStream)
+            {
+                string textLine = text.ReadLine().ToLower();
+                foreach (var dic in wordsCount)
+                {
+                    Regex reg = new(@"\b" + $"{dic.Key}" + @"\b");
+                    MatchCollection matches = reg.Matches(textLine);
+                    foreach (Match match in matches)
+                    {
+                        wordsCount[dic.Key]++;
+                    }
+                }
+
+            }
+
+            using StreamWriter writerWords = new StreamWriter(outputFilePath);
+            foreach (var item in wordsCount.OrderByDescending(x => x.Value))
+            {
+                writerWords.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
