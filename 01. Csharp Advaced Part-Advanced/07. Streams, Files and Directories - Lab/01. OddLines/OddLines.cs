3fa6cd30144f12fe98b20b78231f085d5f14262c
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-03 17:11:24 +0300

    change namespace

---
+++ 01. Csharp Advaced Part-Advanced/07. Streams, Files and Directories - Lab/01. OddLines/OddLines.cs
@@ -0,0 +1,31 @@
+﻿namespace OddLines
+{
+    using System;
+    using System.IO;
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+        public static void ExtractOddLines(string inputFilePath, string
+       outputFilePath)
+        {
+            using StreamReader sr = new StreamReader(inputFilePath);
+            using StreamWriter sw = new StreamWriter(outputFilePath);
+            int count = 0;
+
+            while (!sr.EndOfStream)
+            {
+                string curenLine = sr.ReadLine();
+                if (count % 2 == 1)
+                {
+                    sw.WriteLine(curenLine);
+                }
+                count++;
+            }
+        }
+    }
+}
