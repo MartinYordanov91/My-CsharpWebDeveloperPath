042d80fad1a2f019a2f1ca4e5d83c14993c2e16e
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-03 20:16:39 +0300

    Merge Text Files
    Create

---
+++ 01. Csharp Advaced Part-Advanced/07. Streams, Files and Directories - Lab/04. Merge Text Files/MergeFiles.cs
@@ -0,0 +1,35 @@
+﻿namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+            MergeTextFiles(firstInputFilePath, secondInputFilePath,
+           outputFilePath);
+        }
+        public static void MergeTextFiles(string firstInputFilePath, string
+       secondInputFilePath, string outputFilePath)
+        {
+            using StreamReader srFirst = new StreamReader(firstInputFilePath);
+            using StreamReader srSecond = new StreamReader(secondInputFilePath);
+            using StreamWriter output = new StreamWriter(outputFilePath);
+            while (!srFirst.EndOfStream || !srSecond.EndOfStream)
+            {
+                if (!srFirst.EndOfStream)
+                {
+                    string text = srFirst.ReadLine();
+                    output.WriteLine(text);
+                }
+
+                if (!srSecond.EndOfStream)
+                {
+                    string text = srSecond.ReadLine();
+                    output.WriteLine(text);
+                }
+            }
+        }
+    }
+}
