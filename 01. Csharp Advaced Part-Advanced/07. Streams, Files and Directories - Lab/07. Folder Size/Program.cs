be39de25b2d0203f85c311ba357ee6a45fa182d7
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-03 23:07:07 +0300

    Folder Size
    Create

---
+++ 01. Csharp Advaced Part-Advanced/07. Streams, Files and Directories - Lab/07. Folder Size/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+            GetFolderSize(folderPath, outputPath);
+        }
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            
+            double kilomytes = GetSize(folderPath);
+            using StreamWriter writer = new StreamWriter(outputFilePath);
+            writer.Write($"{kilomytes / 1024} KB");
+        }
+        public static double GetSize(string folderPath)
+        {
+            
+
+            string[] filesPath = Directory.GetFiles(folderPath);
+            double size = 0;
+
+            foreach (string file in filesPath)
+            {
+                FileInfo info = new FileInfo(file);
+                size += info.Length;
+            }
+
+            foreach (string dirPath in Directory.GetDirectories(folderPath))
+            {
+                size += GetSize(dirPath);
+            }
+            return size;
+        }
+    }
+}
