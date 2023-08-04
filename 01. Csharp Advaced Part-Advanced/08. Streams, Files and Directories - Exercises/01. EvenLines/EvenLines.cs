917f1bfe11dac11a6241222821e7547bab3f1c81
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-04 19:01:40 +0300

    Even Lines
    Create

---
+++ 01. Csharp Advaced Part-Advanced/08. Streams, Files and Directories - Exercises/01. EvenLines/EvenLines.cs
@@ -0,0 +1,51 @@
+﻿namespace EvenLines
+{
+    using System.IO;
+    using System;
+    using System.Threading;
+    using System.Text;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder outpput = new StringBuilder();
+            StreamReader inputStreamReader = new StreamReader(inputFilePath);
+            int counter = 0;
+            while (inputStreamReader.EndOfStream == false)
+            {
+                char[] simvols = new char[] { '-', ',', '.', '!', '?' };
+                StringBuilder curentline = new StringBuilder();
+                counter++;
+                string line = inputStreamReader.ReadLine();
+                if (counter % 2 == 1)
+                {
+                    foreach (char c in line)
+                    {
+                        if (simvols.Contains(c))
+                        {
+                            curentline.Append('@');
+                        }
+                        else
+                        {
+                            curentline.Append(c);
+                        }
+                    }
+
+                    string[] words = curentline.ToString().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    Array.Reverse(words);
+                    outpput.AppendLine(string.Join(" " , words));
+                }
+            }
+           return outpput.ToString();
+        }
+    }
+}
