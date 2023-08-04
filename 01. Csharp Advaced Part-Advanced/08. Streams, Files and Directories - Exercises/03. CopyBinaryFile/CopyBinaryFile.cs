69af2ed4604740d251bc123f4f065d68a4956a4e
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-04 20:47:07 +0300

    Copy Binary File
    create

---
+++ 01. Csharp Advaced Part-Advanced/08. Streams, Files and Directories - Exercises/03. CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,25 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using FileStream filerestream = new FileStream(inputFilePath, FileMode.Open);
+            byte[] buffer = new byte[filerestream.Length];
+            filerestream.Read(buffer);
+            using FileStream copyOfPng = new FileStream(outputFilePath,FileMode.OpenOrCreate);
+            copyOfPng.Write(buffer) ;
+        }
+    }
+}
