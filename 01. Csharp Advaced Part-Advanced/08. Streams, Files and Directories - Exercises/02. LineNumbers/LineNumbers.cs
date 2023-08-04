4eb224ef35ed51a163b0957ec721d1688fa3c30d
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-04 19:51:28 +0300

    Line Numbers
    Create

---
+++ 01. Csharp Advaced Part-Advanced/08. Streams, Files and Directories - Exercises/02. LineNumbers/LineNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] text = File.ReadAllLines(inputFilePath);
+            for (int i = 0; i < text.Length; i++)
+            {
+                int countLeters = text[i].Count(c => char.IsLetter(c));
+                int countPunctuation = text[i].Count(c => char.IsPunctuation(c));
+                stringBuilder.AppendLine($"Line {i+1}: {text[i]} ({countLeters})({countPunctuation})");
+            }
+
+            File.WriteAllText(outputFilePath,stringBuilder.ToString());
+        }
+    }
+}
