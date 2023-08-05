69c8c65cf14d10f1d5d245236dc36c952e277a7c
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-05 10:37:53 +0300

    Copy Directory
    Create

---
+++ 01. Csharp Advaced Part-Advanced/08. Streams, Files and Directories - Exercises/05. CopyDirectory/CopyDirectory.cs
@@ -0,0 +1,32 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            string[] dir = Directory.GetDirectories(inputPath);
+            foreach (string dirName in dir)
+            {
+                DirectoryInfo inputPathInfo = new DirectoryInfo(inputPath);
+                string curentPath = dirName;
+                string name = "\\" + inputPathInfo.Name;
+                curentPath = curentPath.Replace(name, string.Empty);
+                Directory.Move(dirName, curentPath);
+            }
+            Directory.Delete(outputPath, true);
+            Directory.Move(inputPath, outputPath);
+
+        }
+    }
+}
