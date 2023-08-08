15de2e419de453de1cedd2be979698d26430b6c7
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-08 22:58:10 +0300

    Add VAT
    Create

---
+++ 01. Csharp Advaced Part-Advanced/09. Functional Programming - Lab/04. Add VAT/AddVAT.cs
@@ -0,0 +1,17 @@
+﻿namespace AddVAT;
+using System;
+internal class AddVAT
+{
+    private static void Main()
+    {
+        double[] prices = Console.ReadLine()
+            .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(double.Parse)
+            .Select(x => 1.2 * x)
+            .ToArray();
+        foreach (var pr in prices)
+        {
+            Console.WriteLine($"{pr:f2}");
+        }
+    }
+}
