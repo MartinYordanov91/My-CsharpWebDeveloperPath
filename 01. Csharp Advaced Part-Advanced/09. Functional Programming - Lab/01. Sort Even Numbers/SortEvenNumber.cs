bd9d18eb2580b74dfd8160e88474e9157a7c0a8f
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-08 22:02:36 +0300

    Sort Even Numbers
    Create

---
+++ 01. Csharp Advaced Part-Advanced/09. Functional Programming - Lab/01. Sort Even Numbers/SortEvenNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace SortEvenNumber;
+
+using System;
+internal class SortEvenNumber
+{
+    private static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .Where(x => x % 2 == 0)
+            .OrderBy(x => x)
+            .ToArray();
+
+        Console.WriteLine(string.Join(", " , numbers));
+        //4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+
+    }
+}
