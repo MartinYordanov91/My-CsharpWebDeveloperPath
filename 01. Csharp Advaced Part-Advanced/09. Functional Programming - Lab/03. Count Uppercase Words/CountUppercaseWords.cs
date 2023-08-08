5da1b2e705a4b3c616c5d4a7f845d29e12366378
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-08 22:43:51 +0300

    Count Uppercase Words
    Create

---
+++ 01. Csharp Advaced Part-Advanced/09. Functional Programming - Lab/03. Count Uppercase Words/CountUppercaseWords.cs
@@ -0,0 +1,18 @@
+﻿namespace CountUppercaseWords;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+internal class CountUppercaseWords
+{
+    private static void Main()
+    {
+        string[] words = Console.ReadLine()
+            .Split(" " , StringSplitOptions.RemoveEmptyEntries)
+            .Where(word => char.IsUpper(word[0]))
+            .ToArray();
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
