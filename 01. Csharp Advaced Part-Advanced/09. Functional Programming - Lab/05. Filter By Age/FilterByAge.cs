83e1936927ee6b77ee48a66607c300ccce95bd6c
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 00:11:36 +0300

    Filter By Age
    Create

---
+++ 01. Csharp Advaced Part-Advanced/09. Functional Programming - Lab/05. Filter By Age/FilterByAge.cs
@@ -0,0 +1,69 @@
+﻿namespace FilterByAge;
+using System;
+using System.Runtime.ConstrainedExecution;
+
+internal class FilterByAge
+{
+    private static void Main()
+    {
+        List<Person> persons = new List<Person>();
+        int countOfPersons = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfPersons; i++)
+        {
+            string[] curentPerson = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            Person person = new(curentPerson[0], int.Parse(curentPerson[1]));
+            persons.Add(person);
+        }
+        string ageDirection = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        Func<Person, bool> filter = p => true;
+        if (ageDirection == "older")
+        {
+            filter = p => p.Age >= age;
+        }
+        else if (ageDirection == "younger")
+        {
+            filter = p => p.Age < age;
+        }
+        else { filter = p => true; }
+
+        List<Person> filteredPersons = persons.Where(filter).ToList();
+
+        string printFrame = Console.ReadLine();
+        Func<Person, string> frameFilter = person => $"{person.Name} - {person.Age}";
+        if (printFrame == "name age")
+        {
+            frameFilter = person => $"{person.Name} - {person.Age}"; // Noah - 31
+        }
+        else if (printFrame == "name")
+        {
+            frameFilter = person => $"{person.Name}"; // Noah 
+        }
+        else if (printFrame == "age")
+        {
+            frameFilter = person => $"{person.Age}";
+        }
+
+        List<string> personAsString = filteredPersons.Select(frameFilter).ToList();
+        foreach (var person in personAsString)
+        {
+            Console.WriteLine(person);
+        }
+    }
+
+}
+
+public class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
