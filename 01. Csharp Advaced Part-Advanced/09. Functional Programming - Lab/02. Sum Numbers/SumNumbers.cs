033f21dbf807ce5c30c84bbe6f6bb5dc2888a737
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-08 22:30:42 +0300

    Sum Numbers
    Create

---
+++ 01. Csharp Advaced Part-Advanced/09. Functional Programming - Lab/02. Sum Numbers/SumNumbers.cs
@@ -0,0 +1,13 @@
+﻿namespace SumNumbers;
+using System;
+internal class SumNumbers
+{
+    private static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+             .Select(int.Parse)
+             .ToArray();
+        Console.WriteLine(numbers.Count());
+        Console.WriteLine(numbers.Sum());
+    }
+}
