7511c26b1f63115327e9c89f0726d8b2e862fc68
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 19:59:57 +0300

    Applied Arithmetics
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/05. Applied Arithmetics/AppliedArithmetics.cs
@@ -0,0 +1,30 @@
+﻿namespace AppliedArithmetics;
+using System;
+internal class AppliedArithmetics
+{
+    private static void Main()
+    {
+        int[] numberArray = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        string comand = string.Empty;
+
+        while ((comand = Console.ReadLine()) != "end")
+        {
+            if (comand == "add")
+            {
+               numberArray= numberArray.Select(x => x += 1).ToArray();
+            }
+            else if (comand == "multiply")
+            {
+                numberArray= numberArray.Select(x => x *= 2).ToArray();
+            }
+            else if (comand == "subtract")
+            {
+                numberArray = numberArray.Select(x => x -= 1).ToArray();
+            }
+            else if(comand == "print")
+            {
+                Console.WriteLine(string.Join(" ", numberArray));
+            }
+        }
+    }
+}
