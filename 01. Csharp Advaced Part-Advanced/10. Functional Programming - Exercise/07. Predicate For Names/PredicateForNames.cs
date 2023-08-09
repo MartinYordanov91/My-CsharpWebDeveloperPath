89c013a72a68e5b652a1dd90b6b48cb497b887c1
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 20:23:03 +0300

    Predicate For Names
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/07. Predicate For Names/PredicateForNames.cs
@@ -0,0 +1,14 @@
+﻿namespace PredicateForNames;
+using System;
+internal class PredicateForNames
+{
+    private static void Main()
+    {
+        int maxLenght = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Where(x => x.Length <=maxLenght)
+            .ToArray();
+        Console.WriteLine(string.Join(Environment.NewLine , names));
+    }
+}
