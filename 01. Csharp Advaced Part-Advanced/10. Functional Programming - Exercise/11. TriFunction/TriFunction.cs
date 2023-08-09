1b3fcd6421e2c947db621b882c531651e9111490
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 21:56:28 +0300

    TriFunction
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/11. TriFunction/TriFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace TriFunction;
+using System;
+internal class TriFunction
+{
+    private static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        string name = names.First(x => x.Sum(n => (int)n) >= num);
+
+        Console.WriteLine(name);
+    }
+}
