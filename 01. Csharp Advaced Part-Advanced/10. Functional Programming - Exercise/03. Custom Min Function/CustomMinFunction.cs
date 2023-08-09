d465b41596aa36cf14431eefbc7f609022fa865a
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 19:41:23 +0300

    Customer min Function
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/03. Custom Min Function/CustomMinFunction.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomMinFunction;
+using System;
+internal class CustomMinFunction
+{
+    private static void Main()
+    {
+        int[] numbers = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        
+        Console.WriteLine(numbers.Min());
+    }
+}
