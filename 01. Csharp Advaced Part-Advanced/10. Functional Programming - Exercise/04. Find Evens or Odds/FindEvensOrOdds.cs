2b1475d40bf724a2efdb4c160fc217e9a0f06c3c
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 19:42:23 +0300

    Find Evens or Odds
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/04. Find Evens or Odds/FindEvensOrOdds.cs
@@ -0,0 +1,24 @@
+﻿namespace FindEvensOrOdds;
+using System;
+internal class FindEvensOrOdds
+{
+    private static void Main()
+    {
+        Predicate<int> isEvens = n => n % 2 == 0;
+        int[] range = Console.ReadLine().Split(" " , StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse).ToArray();
+        string comand = Console.ReadLine();
+
+        for (int number = range[0]; number <= range[1]; number++)
+        {
+            if(comand== "odd")
+            {
+                if (!isEvens(number)) { Console.Write(number + " "); }
+            }
+            else
+            {
+                if (isEvens(number)) { Console.Write(number + " "); }
+            }
+        }
+    }
+}
