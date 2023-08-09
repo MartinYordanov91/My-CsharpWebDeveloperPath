f994a915d1bfa21ae457b3d76861b6502d9a00aa
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 20:16:58 +0300

    Reverse And Exclude
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/06. Reverse And Exclude/ReverseAndExclude.cs
@@ -0,0 +1,16 @@
+﻿namespace ReverseAndExclude;
+using System;
+internal class ReverseAndExclude
+{
+    private static void Main()
+    {
+        int[] masive = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int divisible = int.Parse(Console.ReadLine());
+        Func<int, bool> isValide = (num) => num % divisible == 0;
+        masive = masive.Where(x => !isValide(x)).Reverse().ToArray();
+        Console.WriteLine(string.Join(" " , masive));
+    }
+}
