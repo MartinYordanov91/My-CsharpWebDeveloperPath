84dc3668f595ed0ca74d395aaa68cbc77f313d58
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 17:27:01 +0300

    Action Print
    create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/01. Action Print/ActionPrint.cs
@@ -0,0 +1,11 @@
+﻿namespace ActionPrint;
+using System;
+internal class ActionPrint
+{
+    private static void Main()
+    {
+        Action<string> print = masage => Console.WriteLine(masage);
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        foreach (string name in names) { print(name); }
+    }
+}
