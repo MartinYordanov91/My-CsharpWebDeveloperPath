0c469d095c1fe9e6649fc26a5ebe7aca7a483944
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 18:37:22 +0300

    Knights of Honor
    Create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/02. Knights of Honor/KnightsOfHonor.cs
@@ -0,0 +1,12 @@
+﻿namespace KnightsOfHonor;
+using System;
+internal class KnightsOfHonor
+{
+    private static void Main()
+    {
+        Action<string> print = masage => Console.WriteLine($"Sir {masage}");
+        string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        foreach (string name in names) { print(name); }
+        
+    }
+}
