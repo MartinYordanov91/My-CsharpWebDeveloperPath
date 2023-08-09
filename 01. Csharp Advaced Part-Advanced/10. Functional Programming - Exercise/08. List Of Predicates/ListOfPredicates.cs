bfda5e9b399e839fda05dc4e8739fc3fd50fc503
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-09 20:49:40 +0300

    List Of Predicates
    create

---
+++ 01. Csharp Advaced Part-Advanced/10. Functional Programming - Exercise/08. List Of Predicates/ListOfPredicates.cs
@@ -0,0 +1,25 @@
+﻿namespace ListOfPredicates;
+using System;
+internal class ListOfPredicates
+{
+    private static void Main()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int[] divisors = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        int[] colection = Enumerable.Range(1, num).Where(x =>
+        {
+
+            foreach (int n in divisors)
+            {
+                if (x % n != 0) { return false; }
+            }
+            return true;
+        })
+            .ToArray();
+
+        Console.WriteLine(string.Join(" ", colection));
+    }
+}
