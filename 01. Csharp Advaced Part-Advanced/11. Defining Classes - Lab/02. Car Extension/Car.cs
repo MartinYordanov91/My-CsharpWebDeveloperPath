32b665f295e164b42c97aba92daa69ff61bd5854
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-10 22:47:28 +0300

    Car Extension
    Adding solved problem from topic: Defining Classes - Lab

---
+++ 01. Csharp Advaced Part-Advanced/11. Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distace)
+        {
+            if (distace * FuelConsumption <= FuelQuantity)
+            {
+                FuelQuantity -= distace * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}");
+
+            
+            return sb.ToString();
+        }
+    }
+}
