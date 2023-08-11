5c6a39f02ef3e6614b6be93f790ea69f58d053ba
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-11 23:56:59 +0300

    Special Car
    adding

---
+++ 01. Csharp Advaced Part-Advanced/11. Defining Classes - Lab/05. Special Cars/Car.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+        : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            Tires = tires;
+            Engine = engine;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+
+
+        public void Drive(double distace)
+        {
+            if ((distace / 100) * FuelConsumption <= FuelQuantity)
+            {
+                FuelQuantity -= (distace / 100) * FuelConsumption;
+                
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:f2}");
+            return sb.ToString();
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"HorsePowers: {this.Engine.HorsePower}");
+            sb.Append($"FuelQuantity: {this.FuelQuantity}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/11. Defining Classes - Lab/05. Special Cars/StartUp.cs
@@ -0,0 +1,65 @@
+﻿namespace CarManufacturer;
+public class StartUp
+{
+    static void Main()
+    {
+        List<List<Tire>> tiresSets = new List<List<Tire>>();
+        List<Engine> engineSets = new List<Engine>();
+        List<Car> carColections = new List<Car>();
+
+        string argoment = string.Empty;
+        while ((argoment = Console.ReadLine()) != "No more tires")
+        {
+            string[] curentArg = argoment.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<Tire> curentSet = new List<Tire>();
+            for (int i = 0; i < curentArg.Length; i += 2)
+            {
+                int year = int.Parse(curentArg[i]);
+                double pressure = double.Parse(curentArg[i + 1]);
+                curentSet.Add(new Tire(year, pressure));
+            }
+            tiresSets.Add(curentSet);
+        }
+
+        argoment = string.Empty;
+        while ((argoment = Console.ReadLine()) != "Engines done")
+        {
+            string[] curentArg = argoment.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int horsePower = int.Parse(curentArg[0]);
+            double cubicCapacity = double.Parse(curentArg[1]);
+
+            Engine curentEngine = new Engine(horsePower, cubicCapacity);
+            engineSets.Add(curentEngine);
+        }
+
+
+        argoment = string.Empty;
+        while ((argoment = Console.ReadLine()) != "Show special")
+        {
+            string[] curentArg = argoment.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string make = curentArg[0];
+            string model = curentArg[1];
+            int year = int.Parse(curentArg[2]);
+            double fuelQuantity = double.Parse(curentArg[3]);
+            double fuelConsumption = double.Parse(curentArg[4]);
+            Engine neadEngine = engineSets[int.Parse(curentArg[5])];
+            Tire[] neadTires = tiresSets[int.Parse(curentArg[6])].ToArray();
+
+            Car curentCar = new Car(make, model, year, fuelQuantity, fuelConsumption, neadEngine, neadTires);
+            carColections.Add(curentCar);
+        }
+
+        List<Car> specialCars = carColections
+                .FindAll(c => c.Year >= 2017
+                           && c.Engine.HorsePower > 330
+                           && c.Tires.Select(t => t.Pressure).Sum() >= 9
+                           && c.Tires.Select(t => t.Pressure).Sum() <= 10);
+
+        foreach (var car in specialCars)
+        {
+            car.Drive(20);
+            Console.WriteLine(car);
+        }
+
+    }
+}
