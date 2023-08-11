98b72ecb1ecf9f295f847257a078eb338c1ed01d
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-11 19:38:06 +0300

    Car Constructors
    Adding constructors

---
+++ 01. Csharp Advaced Part-Advanced/11. Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+        : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) :this(make , model , year) 
+        {
+            
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distace)
+        {
+            if (distace * FuelConsumption <= FuelQuantity)
+            {
+                FuelQuantity -= distace * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.AppendLine($"Fuel: {this.FuelQuantity:F2}");
+
+
+            return sb.ToString();
+        }
+    }
+}
