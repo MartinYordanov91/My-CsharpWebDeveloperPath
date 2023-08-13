c329ad957404069c1b8f9a1f670331b8a6c7d8ea
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-13 23:19:17 +0300

    adding Raw Data
    Resolved Task

---
+++ 01. Csharp Advaced Part-Advanced/12. Defining Classes - Exercise/07_RawData/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Car
+    {
+        public Car(
+            string model, 
+            int speed,
+            int power,
+            int weight,
+            string type,
+            double pressure1,
+            int age1,
+            double pressure2,
+            int age2,
+            double pressure3,
+            int age3,
+            double pressure4,
+            int age4
+            )
+        {
+            Model = model;
+            Engine = new Engine(speed, power);
+            Cargo = new Cargo(weight, type);
+            Tires = new Tires[4];
+            Tires[0] = new Tires(pressure1 , age1);
+            Tires[1] = new Tires(pressure2 , age2);
+            Tires[2] = new Tires(pressure3 , age3);
+            Tires[3] = new Tires(pressure4 , age4);
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public Cargo Cargo { get; set; }
+
+        public Tires[] Tires { get; set; }
+
+        public override string ToString()
+        {
+            return Model;
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/12. Defining Classes - Exercise/07_RawData/StartUp.cs
@@ -0,0 +1,60 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>();
+        int countOfCars = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < countOfCars; i++)
+        {
+            string[] carParameters = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Car car = new Car
+                (
+                    carParameters[0],                  /// model
+                    int.Parse(carParameters[1]),       /// speed
+                    int.Parse(carParameters[2]),       /// power
+                    int.Parse(carParameters[3]),       /// weight
+                    carParameters[4],                  /// type
+                    double.Parse(carParameters[5]),    /// pressure
+                    int.Parse(carParameters[6]),       /// age
+                    double.Parse(carParameters[7]),    /// pressure
+                    int.Parse(carParameters[8]),       /// age
+                    double.Parse(carParameters[9]),    /// pressure
+                    int.Parse(carParameters[10]),      /// age
+                    double.Parse(carParameters[11]),   /// pressure
+                    int.Parse(carParameters[12])       /// age
+                );
+
+            cars.Add(car);
+        }
+
+        string cargoType = Console.ReadLine();
+        List<Car> filteredCar = new List<Car>();
+
+        if (cargoType == "fragile")
+        {
+            filteredCar = cars
+                .Where
+                     (
+                         x => x.Cargo.Type == "fragile" &&
+                         x.Tires.Any(x => x.Pressure < 1)
+                     )
+                .ToList();
+        }
+        else if (cargoType == "flammable")
+        {
+            filteredCar = cars
+                .Where
+                    (
+                        x => x.Cargo.Type == "flammable" &&
+                        x.Engine.Power > 250
+                    )
+                .ToList();
+        }
+
+        Console.WriteLine(string.Join(Environment.NewLine , filteredCar));
+    }
+}
