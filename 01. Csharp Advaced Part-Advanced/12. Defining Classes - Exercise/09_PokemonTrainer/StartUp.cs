cbb8bfc4e4b6dccd7eed4c27a18a049b5c44eed6
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-15 20:06:13 +0300

    Pokemon Trainer
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/12. Defining Classes - Exercise/09_PokemonTrainer/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            string comand = string.Empty;
+
+            while ((comand = Console.ReadLine()) != "Tournament")
+            {
+                string[] infoArg = comand
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = infoArg[0];
+                string pokemonName = infoArg[1];
+                string element = infoArg[2];
+                int healt = int.Parse(infoArg[3]);
+
+                Pokemon pokemon = new Pokemon(pokemonName, element, healt);
+
+                if (trainers.Any(x => x.Name == trainerName) == false)
+                {
+                    Trainer trainer = new Trainer(trainerName);
+                    trainer.Pokemons.Add(pokemon);
+                    trainers.Add(trainer);
+                }
+                else
+                {
+                    Trainer trainer = trainers.First(x => x.Name == trainerName);
+                    trainer.Pokemons.Add(pokemon);
+                }
+
+            }
+
+            while ((comand = Console.ReadLine()) != "End")
+            {
+                foreach (var traner in trainers)
+                {
+                    if (traner.Pokemons.Any(x => x.Element == comand))
+                    {
+                        traner.Badges++;
+                    }
+                    else
+                    {
+                        traner.Pokemons.ForEach(x => x.Health -= 10);
+                    }
+
+                    while (traner.Pokemons.Any(x => x.Health <= 0))
+                    {
+                        Pokemon death = traner.Pokemons.First(x => x.Health <= 0);
+                        traner.Pokemons.Remove(death);
+                    }
+                }
+            }
+
+            foreach (var trainer in trainers.OrderByDescending(x =>x.Badges))
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/12. Defining Classes - Exercise/09_PokemonTrainer/Trainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {Pokemons.Count}";
+        }
+    }
+}
