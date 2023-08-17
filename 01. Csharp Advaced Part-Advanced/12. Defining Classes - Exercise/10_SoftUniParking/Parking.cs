27e0fb20a87c33cbcfcd67f40a9a8358a2e93ee6
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-17 21:18:40 +0300

    adding parking

---
+++ 01. Csharp Advaced Part-Advanced/12. Defining Classes - Exercise/10_SoftUniParking/Parking.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        
+        private int capacity;
+
+        public Parking(int capacity)
+        {
+            this.capacity = capacity;
+
+            Cars = new();
+        }
+
+        public List<Car> Cars { get; set; }
+        public int Count { get { return Cars.Count; } }
+
+        public string AddCar(Car car)
+        {
+
+            if (Cars.Any(x => x.RegistrationNumber == car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+
+            if (capacity <= Cars.Count)
+            {
+                return "Parking is full!";
+            }
+
+            Cars.Add(car);
+            return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+
+        }
+        public string RemoveCar(string registration)
+        {
+            if (Cars.Any(x => x.RegistrationNumber == registration) == false)
+            {
+                return $"Car with that registration number, doesn't exist!";
+            }
+
+            Car car = Cars.First(x => x.RegistrationNumber == registration);
+            Cars.Remove(car);
+            return $"Successfully removed {registration}";
+        }
+
+        public Car GetCar(string registration)
+        {
+            Car car = Cars.First(x => x.RegistrationNumber == registration);
+            return car;
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (var number in registrationNumbers)
+            {
+               RemoveCar(number);
+            }
+        }
+    }
+}
