37e9d4a8ec29f9b64d202b77fcf2da6cfc275105
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-21 19:45:05 +0300

    Implement Custom List basic
    Add

---
+++ 01. Csharp Advaced Part-Advanced/13. ImplementSomeFunction/ImplementList/CustomList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList
+    {
+        private const int initiallyCapacity = 2;
+        public CustomList()
+        {
+            Items = new int[initiallyCapacity];
+            Count = 0;
+        } 
+        public int[] Items { get; set; }
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                AutOfrangeError(index);
+                return Items[index];
+            }
+            set
+            {
+                AutOfrangeError(index);
+                Items[index] = value;
+            }
+        }
+        public void Add(int item)
+        {
+            Resize();
+            Items[Count++] = item;
+        }
+        public int RemoveAt(int index)
+        {
+            AutOfrangeError(index);
+            int remulved = Items[index];
+            Items[index] = default;
+            ShiftLeft(index);
+            Count--;
+            Items[Count] = default;
+            Shrink();
+            return remulved;
+        }
+        public void Reverse()
+        {
+            int[] copy = new int[Items.Length];
+            int num = Count-1;
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = Items[num--];
+            }
+            Items = copy;
+        }
+        public void InsertAt(int index, int item)
+        {
+            AutOfrangeError(index);
+            Resize();
+            ShiftRight(index);
+            Items[index] = item;
+            Count++;
+        }
+        public bool Contains(int element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Items[i] == element)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            AutOfrangeError(firstIndex);
+            AutOfrangeError(secondIndex);
+
+            int firstElement = Items[firstIndex];
+            Items[firstIndex] = Items[secondIndex];
+            Items[secondIndex] = firstElement;
+        }
+        private void Shrink()
+        {
+            if (Count <= Items.Length / 4)
+            {
+                int[] copy = new int[Items.Length / 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    copy[i] = Items[i];
+                }
+
+                Items = copy;
+            }
+        }
+        private void ShiftRight(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                Items[i + 1] = Items[i];
+            }
+        }
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                Items[i] = Items[i + 1];
+            }
+        }
+        private void AutOfrangeError(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Пич Излезнал си от ренджа на масива");
+            }
+        }
+        private void Resize()
+        {
+            if (Count == Items.Length)
+            {
+                int[] copy = new int[Items.Length * 2];
+
+                for (int i = 0; i < Count; i++)
+                {
+                    copy[i] = Items[i];
+                }
+
+                Items = copy;
+            }
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/13. ImplementSomeFunction/ImplementList/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace CustomList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CustomList list = new CustomList();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(9);
+            list.RemoveAt(2);
+            list.RemoveAt(2);
+            list.RemoveAt(2);
+            list.RemoveAt(2);
+            list.RemoveAt(2);
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            list.InsertAt(1, 111);
+            Console.WriteLine(list.Contains(9));
+            Console.WriteLine(list.Contains(99));
+            list.Swap(0, 2);
+            list.Reverse();
+            ;
+        }
+    }
+}
