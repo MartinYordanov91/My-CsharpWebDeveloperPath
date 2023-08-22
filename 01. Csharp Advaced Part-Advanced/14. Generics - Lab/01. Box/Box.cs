4e4e717500b9b4760431216d60db1f9303a350cc
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-22 19:51:18 +0300

    Generics Box
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/14. Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            List = new List<T>();
+            Count = 0;
+        }
+
+        public List<T> List { get; set; }
+        public int Count { get; private set; }
+
+        public void Add(T elements)
+        {
+            List.Add(elements);
+            Count++;
+        }
+        public T Remove()
+        {
+            T topElement = List[Count - 1];
+            List.RemoveAt(Count - 1);
+            Count--;
+            return topElement;
+        }
+    }
+}
