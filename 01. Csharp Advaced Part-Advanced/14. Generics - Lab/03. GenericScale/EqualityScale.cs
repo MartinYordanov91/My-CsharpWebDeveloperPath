96a8bc139829c53e5a48c13f098231d6d4b616ac
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-22 21:23:24 +0300

    Generic Scale
    adding

---
+++ 01. Csharp Advaced Part-Advanced/14. Generics - Lab/03. GenericScale/EqualityScale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> 
+    {
+        private T Left {get;set;}
+        private T Right { get; set; }
+        public EqualityScale(T left , T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/14. Generics - Lab/03. GenericScale/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            EqualityScale<int> dafds = new EqualityScale<int>(5, 6);
+            Console.WriteLine(dafds.AreEqual());
+        }
+    }
+}
