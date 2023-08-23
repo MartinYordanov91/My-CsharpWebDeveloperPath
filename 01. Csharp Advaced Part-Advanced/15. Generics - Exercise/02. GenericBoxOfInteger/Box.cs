3028aa62bdee24165899e57c5f1c03cb1cfa273d
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-23 19:04:20 +0300

    Generic Box of Integer
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/02. GenericBoxOfInteger/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            string className = typeof(T).FullName;
+            return $"{className}: {Value}";
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/02. GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int countOfText = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfText; i++)
+            {
+                int curentText = int.Parse(Console.ReadLine());
+                Box<int> text = new Box<int>(curentText);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
