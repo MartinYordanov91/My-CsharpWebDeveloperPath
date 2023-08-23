f52a59d6a1777cc3730df5ccf529e6a7bd55c729
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-23 18:40:28 +0300

    Generic Box of String
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/01. GenericBoxOfString/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace GenericBoxОfString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int countOfText = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfText; i++)
+            {
+                string curentText = Console.ReadLine();
+                Box<string> text = new Box<string>(curentText);
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
