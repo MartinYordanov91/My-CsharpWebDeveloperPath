2337bc78667e3c7415ac5d77b7f8beb45bfa838a
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-23 20:17:48 +0300

    Generic Swap Method Integer
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/04. GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> list = new List<string>();
+            int countOfText = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfText; i++)
+            {
+                int curentText = int.Parse(Console.ReadLine());
+                Box<int> text = new Box<int>(curentText);
+                list.Add(text.ToString());
+            }
+            int[] index = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Box<string> swaping = new Box<string>();
+            list = swaping.Swap(list, index[0], index[1]);
+
+            foreach (var x in list)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
