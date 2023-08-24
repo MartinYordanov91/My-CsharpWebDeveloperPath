194d822419ad34ca567ae8915fa8828687bb734f
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-24 21:11:33 +0300

    create tuple of 3 elements
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/08. Threeuple/CustomTuple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threeuple
+{
+    public class CustomTuple<T1, T2 , T3>
+    {
+        private readonly T1 item1;
+        private readonly T2 item2;
+        private readonly T3 item3;
+
+        public CustomTuple(T1 item1, T2 item2, T3 item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3; 
+        }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2} -> {item3}";
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/08. Threeuple/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] nameAdres = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, string , string> firstLine = new($"{nameAdres[0]} {nameAdres[1]}", nameAdres[2], string.Join(" ",nameAdres[3..]));
+
+            string[] personBeer = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, int , bool> secondLine = new(personBeer[0], int.Parse(personBeer[1]), personBeer[2] == "drunk");
+
+            string[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, double , string> theeLines = new(numbers[0], double.Parse(numbers[1]), numbers[2]);
+
+            Console.WriteLine(firstLine);
+            Console.WriteLine(secondLine);
+            Console.WriteLine(theeLines);
+        }
+    }
+}
