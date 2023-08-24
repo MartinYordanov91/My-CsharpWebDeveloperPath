f5fdc85398d29a5340fc13c76214bef8f69cad66
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-24 21:01:49 +0300

    Custom Inplementate Tuple
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace Tuple
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] nameAdres = Console.ReadLine().Split(" " , StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, string> firstLine = new($"{nameAdres[0]} {nameAdres[1]}" , nameAdres[2]);
+
+            string[] personBeer = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<string, int> secondLine = new(personBeer[0] , int.Parse(personBeer[1]));
+
+            string[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            CustomTuple<int, double> theeLines = new(int.Parse(numbers[0]), double.Parse(numbers[1]));
+
+            Console.WriteLine(firstLine);
+            Console.WriteLine(secondLine);
+            Console.WriteLine(theeLines);
+        }
+    }
+}
