e455b942ff09de1eda39d55b038aeec9431d828f
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-24 20:40:44 +0300

    Generic Count Method Double
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/06. GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        List<T> items = new List<T>();  
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public int Count(T text)
+        {
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (item.CompareTo(text) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/06. GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace GenericCountMethodDoubles
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+           Box<double> box = new Box<double>();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                box.Add(number);
+            }
+
+            double item = double.Parse(Console.ReadLine());
+            Console.WriteLine(box.Count(item));
+
+        }
+      
+    }
+}
