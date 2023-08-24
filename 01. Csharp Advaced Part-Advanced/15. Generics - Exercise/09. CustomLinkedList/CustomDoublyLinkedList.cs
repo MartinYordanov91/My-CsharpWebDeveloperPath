b43003e20bb40c7abc2ad29235910e075f86ed03
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-24 23:12:44 +0300

    Custom Double Linked List  Generic
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/09. CustomLinkedList/CustomDoublyLinkedList.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace CustomLinkedList;
+
+public class CustomDoublyLinkedList<T>
+{
+    private class Node
+    {
+        public Node(T value)
+        {
+            Value = value;
+
+        }
+
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+        public T Value { get; set; }
+    }
+   
+    private Node head;
+    private Node tail;
+    public int Count { get; private set; }
+
+    public void AddFirst(T number)
+    {
+        Node newNode = new(number);
+
+        if (Count == 0)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode.Next = head;
+            head.Previous = newNode;
+            head = newNode;
+        }
+        Count++;
+    }
+    public void Add(T number)
+    {
+        Node newNode = new(number);
+
+        if (Count == 0)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            newNode.Next = head;
+            head.Previous = newNode;
+            head = newNode;
+        }
+        Count++;
+    }
+    public void AddLast(T number)
+    {
+        Node newNode = new Node(number);
+        if (Count == 0)
+        {
+            tail = head = newNode;
+        }
+        else
+        {
+            newNode.Previous = tail;
+            tail.Next = newNode;
+            tail = newNode;
+        }
+        Count++;
+    }
+    public T RemoveFirst()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("The list is empty");
+        }
+
+        T firstElement = head.Value;
+        Node newHead = head.Next;
+
+        if (Count == 1)
+        {
+            head = tail = null;
+        }
+        else
+        {
+            newHead.Previous = null;
+            head = newHead;
+        }
+        Count--;
+
+        return firstElement;
+    }
+    public T Remove()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("The list is empty");
+        }
+
+        T firstElement = head.Value;
+        Node newHead = head.Next;
+
+        if (Count == 1)
+        {
+            head = tail = null;
+        }
+        else
+        {
+            newHead.Previous = null;
+            head = newHead;
+        }
+        Count--;
+
+        return firstElement;
+    }
+    public T RemoveLast()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException("The list is empty");
+        }
+        T lastElement = tail.Value;
+        Node newTail = tail.Previous;
+
+        if (Count == 1)
+        {
+            tail = head = null;
+        }
+        else
+        {
+            newTail.Next = null;
+            tail = newTail;
+        }
+        Count--;
+        return lastElement;
+    }
+    public void ForEach(Action<T> action)
+    {
+        Node note = head;
+        while (note != null)
+        {
+            action(note.Value);
+            note = note.Next;
+        }
+    }
+    public T[] ToArray()
+    {
+        T[] array = new T[Count];
+
+
+        var note = head;
+        for (int i = 0; i < Count; i++)
+        {
+            array[i] = note.Value;
+            note = note.Next;
+        }
+        return array;
+    }
+}
+
+
---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/09. CustomLinkedList/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using CustomLinkedList;
+using System;
+
+CustomDoublyLinkedList<int> intList = new();
+intList.AddFirst(3);
+intList.AddFirst(2);
+intList.AddFirst(1);
+intList.AddLast(4);
+
+Console.WriteLine(intList.RemoveFirst());
+Console.WriteLine(intList.RemoveLast());
+int[] arr = intList.ToArray();
+
+intList.ForEach(x => Console.Write(x + " "));
+
+Console.WriteLine();
+Console.WriteLine(intList.Count);
+
+CustomDoublyLinkedList<string> StringList = new();
+StringList.AddFirst("some");
+StringList.AddFirst("random");
+StringList.AddFirst("strings");
+StringList.AddLast("Adding");
+
+StringList.ForEach(x => Console.Write(x + " "));
+
+Console.WriteLine();
+Console.WriteLine(StringList.Count);
