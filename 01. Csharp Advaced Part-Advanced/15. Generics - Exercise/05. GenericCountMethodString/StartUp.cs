2b9cf7675d6996d283a4199cd6a7fa8c1a294848
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-24 20:20:55 +0300

    Generic Count Method String
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/15. Generics - Exercise/05. GenericCountMethodString/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace GenericCountMethodString
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<string> integers = new List<string>();
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                string number = Console.ReadLine();
+                integers.Add(number);
+            }
+
+            string indexses = Console.ReadLine();
+            Console.WriteLine(Count<string>(integers, indexses));
+            
+        }
+        static int Count<T>(List<T> list ,string text)
+        {
+            int count = 0;
+            foreach (var item in list)
+            {
+                if (text.CompareTo(item) < 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
