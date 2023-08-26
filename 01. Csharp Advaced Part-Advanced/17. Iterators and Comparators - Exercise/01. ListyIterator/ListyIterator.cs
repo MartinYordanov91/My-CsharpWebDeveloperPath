ebbfe5c4a0f3d58691c672029f1701f7617b5c04
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-26 20:06:42 +0300

    List Eterator
    adding

---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int index = 0;
+        private List<T> elements;
+
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+        }
+        public bool Move()
+        {
+
+            if (index + 1 < elements.Count)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext() => this.index < this.elements.Count - 1;
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/01. ListyIterator/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<string> create = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();
+            ListyIterator<string> listy = new(create);
+
+            string comand = string.Empty;
+            while ((comand = Console.ReadLine())!="END")
+            {
+                switch (comand)
+                {
+                    case "Move":
+                        Console.WriteLine(listy.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listy.Print();
+                        }
+                        catch (Exception ex) 
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        }
+                        
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listy.HasNext());
+                        break;
+                }
+            }
+
+        }
+    }
+}
