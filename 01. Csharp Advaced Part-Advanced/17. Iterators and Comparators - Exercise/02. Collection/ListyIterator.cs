4359079538f660c17b4288d77c1dbda6bb10f0ef
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-26 20:08:57 +0300

    Colletion
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/02. Collection/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private int index = 0;
+        private List<T> elements;
+
+      
+
+        public ListyIterator(List<T> elements)
+        {
+            this.elements = elements;
+        }
+        public bool Move()
+        {
+
+            if (index + 1 < elements.Count)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext() => this.index < this.elements.Count - 1;
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(elements[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var element in elements)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()=>this.GetEnumerator();
+      
+    }
+}
