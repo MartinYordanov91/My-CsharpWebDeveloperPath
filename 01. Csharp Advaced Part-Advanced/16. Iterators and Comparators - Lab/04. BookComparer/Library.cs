b39a88fb705c04381bedce9c3a7f4cd9085be4c4
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-26 18:44:22 +0300

    Book Comparer
    adding

---
+++ 01. Csharp Advaced Part-Advanced/16. Iterators and Comparators - Lab/04. BookComparer/Library.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books.OrderBy(x => x ,new BookComparator()).ToList());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private int index;
+            private readonly List<Book> books;
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = books;
+            }
+            public Book Current => this.books[this.index];
+            object IEnumerator.Current => this.Current;
+            public bool MoveNext() => ++this.index < this.books.Count;
+            public void Reset() => this.index = -1;
+            public void Dispose() { }
+
+        }
+    }
+   
+}
