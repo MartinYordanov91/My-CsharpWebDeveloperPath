761683dc7eab40dcc630a255fed957a750fd9e53
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-26 20:48:09 +0300

    stack
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/03. Stack/CustomStack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private const int zetoIndex = 0;
+        private List<T> elements = new();
+        public int Count { get { return elements.Count; } }
+        public void Push(T element)
+        {
+            elements.Insert(zetoIndex, element);
+        }
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            T remolvElement = elements[zetoIndex];
+            elements.RemoveAt(zetoIndex);
+            return remolvElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in elements)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()=>this.GetEnumerator();
+        
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/03. Stack/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Stack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CustomStack<int> integers = new();
+
+            string comand = string.Empty;
+            while ((comand = Console.ReadLine()) != "END")
+            {
+                string[] tocans = comand
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                if (tocans[0] == "Push")
+                {
+                    int[] ints = tocans.Skip(1).Select(int.Parse).ToArray();
+                    foreach (int i in ints)
+                    {
+                        integers.Push(i);
+                    }
+                }
+                else if (tocans[0] == "Pop")
+                {
+                    try
+                    {
+                        integers.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in integers)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
