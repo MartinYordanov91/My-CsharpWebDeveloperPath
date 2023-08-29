344229898951c187fafdf603e38e1e83d0fd141e
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-29 17:54:24 +0300

    Froggy
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/04. Froggy/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(", " , StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Lake<int> frogi = new(list);
+            Console.WriteLine(string.Join(", " , frogi));
+        }
+    }
+}
