6d8c82bad82b79640d0464a1d1553cd3768dfe92
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-31 22:37:06 +0300

    Custom Comparator

---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/07. CustomComparator/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace CustomComparator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] ints = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(ints, new CustomComperator());
+
+            Console.WriteLine(string.Join(" " , ints));
+        }
+
+        public class CustomComperator : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                if (x % 2 == 0 && y % 2 != 0)
+                {
+                    return -1;
+                }
+
+                if (x % 2 != 0 && y % 2 == 0)
+                {
+                    return 1;
+                }
+                return x.CompareTo(y);
+            }
+        }
+
+    }
+}
