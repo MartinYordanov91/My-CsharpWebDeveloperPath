0c46703762a5a89fb173557cc6a12ad12ea00ed2
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-08-31 22:18:05 +0300

    Equality Logic

---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/06. EqualityLogic/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingObjects
+{
+    public class Person: IComparable<Person>
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+           
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public int CompareTo(Person other)
+        {
+            if(Name.CompareTo(other.Name) != 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+
+            return Age.CompareTo(other.Age);
+        }
+
+        public override bool Equals(object? obj)
+        {
+           Person person = obj as Person;
+
+           if(person == null) return false;
+
+           return Name.Equals(person.Name) && Age.Equals(person.Age);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = Name.GetHashCode() + Age.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/17. Iterators and Comparators - Exercise/06. EqualityLogic/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using ComparingObjects;
+
+namespace EqualityLogic
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            HashSet<Person> hashPersons = new();
+            SortedSet<Person> sortPerson = new();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] curentPerson = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new(curentPerson[0], int.Parse(curentPerson[1]));
+                sortPerson.Add(person);
+                hashPersons.Add(person);
+            }
+
+            Console.WriteLine(hashPersons.Count());
+            Console.WriteLine(sortPerson.Count());
+        }
+    }
+}
