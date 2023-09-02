6cf65473b548e0467c575b704086251ff5cddd0e
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-02 13:30:21 +0300

    Recursive Array Sum
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/18. Basic Algorithms - Exercise/01. RecursiveArraySum/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+
+namespace RecursiveArraySum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arrayForSum = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int sum = RecursionSum(arrayForSum, 0);
+            Console.WriteLine(sum);
+
+
+        }
+
+        private static int RecursionSum(int[] ints, int index)
+        {
+
+            if (ints.Length - 1 == index)
+            {
+                return ints[index];
+            }
+
+            int num = ints[index++];
+
+            return num + RecursionSum(ints, index);
+        }
+    }
+}
