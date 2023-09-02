5a3ab191119708b7159df998806ccb61b0a66ec1
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-02 14:06:03 +0300

    Recursive Factoria
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/18. Basic Algorithms - Exercise/02. RecursiveFactorial/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace RecursiveFactorial
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int factorialNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CustomRecursiveFactorial(factorialNumber));
+        }
+        private static long CustomRecursiveFactorial(int value)
+        {
+            if (value <= 1)
+            {
+                return 1;
+            }
+
+            int number = value --;
+
+            return number * CustomRecursiveFactorial(value);
+        }
+    }
+}
