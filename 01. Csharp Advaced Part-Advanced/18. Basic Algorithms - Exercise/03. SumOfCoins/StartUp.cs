dd52ef6a026087a8dcf0f3a30ae88da1f4b20076
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-02 15:01:51 +0300

    Sum of Coins
    Adding

---
+++ 01. Csharp Advaced Part-Advanced/18. Basic Algorithms - Exercise/03. SumOfCoins/StartUp.cs
@@ -0,0 +1,73 @@
+﻿namespace SumOfCoins
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int targetSum = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Dictionary<int, int> selectionCoin = ChooseCoin(list, targetSum);
+
+                Console.WriteLine($"Number of coins to take: {selectionCoin.Values.Sum()}");
+
+                foreach (var select in selectionCoin)
+                {
+                    Console.WriteLine($"{select.Value} coin(s) with value {select.Key}");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static Dictionary<int, int> ChooseCoin(List<int> coins, int targetSum)
+        {
+            Dictionary<int, int> choosencCoin = new();
+            coins = coins.OrderByDescending(x => x).ToList();
+            //int curentsum = 0;
+
+            while (targetSum != 0)
+            {
+                if(targetSum / coins.First() > 1)
+                {
+                    int value = targetSum / coins.First();
+                    int key = coins.First();
+
+                    if (choosencCoin.ContainsKey(key) == false)
+                    {
+                        choosencCoin[key] = 0;
+                    }
+                    choosencCoin[key] = value;
+                    targetSum -= key * value;
+                }
+
+                foreach (int coin in coins)
+                {
+                    if (coin <= targetSum)
+                    {
+                        if (choosencCoin.ContainsKey(coin) == false)
+                        {
+                            choosencCoin[coin] = 0;
+                        }
+                        choosencCoin[coin] ++;
+                        targetSum -= coin;
+                       break;
+                    }
+                }
+
+                if(targetSum >0  && coins.Last() > targetSum)
+                {
+                    throw  new InvalidOperationException("Error");
+                }
+            }
+            return choosencCoin;
+        }
+    }
+}
