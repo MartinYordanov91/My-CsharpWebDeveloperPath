3bae6e2177c2f2b38785ed4860166195e8094663
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-03 10:46:12 +0300

    Monster Extermination
    Stack and Queue

---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Exam Retake – 09 August 2023/01. MonsterExtermination/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace MonsterExtermination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mopnstersArmor = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] heroDamage = Console.ReadLine()
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queueArmor = new(mopnstersArmor);
+            Stack<int> stackDamage = new(heroDamage);
+            int monsterCiled = 0;
+
+            while (queueArmor.Any() && stackDamage.Any())
+            {
+                if (stackDamage.Peek() >= queueArmor.Peek())
+                {
+                    monsterCiled++;
+                    int damage = stackDamage.Pop();
+
+                    if (stackDamage.Any())
+                    {
+                        int nextDamage = stackDamage.Pop() + (damage - queueArmor.Dequeue());
+                        stackDamage.Push(nextDamage);
+                        continue;
+                    }
+
+                    if (damage - queueArmor.Peek() != 0)
+                    {
+                        stackDamage.Push(damage - queueArmor.Dequeue());
+                        continue;
+                    }
+                    else queueArmor.Dequeue();
+
+                }
+                else
+                {
+                    int curentArmor = queueArmor.Dequeue() - stackDamage.Pop();
+                    queueArmor.Enqueue(curentArmor);
+                    
+                }
+            }
+
+            if (queueArmor.Any() == false)
+            {
+                Console.WriteLine("All monsters have been killed!");
+            }
+
+            if (stackDamage.Any() == false)
+            {
+                Console.WriteLine("The soldier has been defeated.");
+            }
+
+            Console.WriteLine($"Total monsters killed: {monsterCiled}");
+        }
+    }
+}
