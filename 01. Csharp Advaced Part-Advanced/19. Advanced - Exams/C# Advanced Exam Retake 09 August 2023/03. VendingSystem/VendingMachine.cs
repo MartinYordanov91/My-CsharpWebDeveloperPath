5579ec013a69e8a1502557a1e5e1a2ea653d6aa6
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-05 20:06:33 +0300

    adding

---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Exam Retake 09 August 2023/03. VendingSystem/VendingMachine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            ButtonCapacity = buttonCapacity;
+            Drinks = new();
+        }
+
+        public int ButtonCapacity { get; set; }
+        public List<Drink> Drinks { get; set; }
+        public int GetCount { get => this.Drinks.Count; }
+
+        public void AddDrink(Drink drink)
+        {
+            if (this.ButtonCapacity > this.Drinks.Count)
+            {
+                this.Drinks.Add(drink);
+            }
+        }
+        public bool RemoveDrink(string name)
+        {
+            if (this.Drinks.Any(n => n.Name == name))
+            {
+                Drink remulve = this.Drinks.First(n => n.Name == name);
+                this.Drinks.Remove(remulve);
+                return true;
+            }
+            return false;
+        }
+        public Drink GetLongest()
+        {
+
+            Drink bigVolume = this.Drinks.OrderByDescending(v => v.Volume).FirstOrDefault();
+            return bigVolume;
+        }
+        public Drink GetCheapest()
+        {
+
+            Drink bigPrice = this.Drinks.OrderBy(v => v.Price).FirstOrDefault();
+            return bigPrice;
+        }
+        public string BuyDrink(string name)
+        {
+
+            Drink bigspecific = this.Drinks.FirstOrDefault(v => v.Name == name);
+            return bigspecific.ToString();
+        }
+        public string Report()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("Drinks available:");
+            foreach (Drink drink in this.Drinks)
+            {
+                sb.AppendLine(drink.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+
+    }
+}
