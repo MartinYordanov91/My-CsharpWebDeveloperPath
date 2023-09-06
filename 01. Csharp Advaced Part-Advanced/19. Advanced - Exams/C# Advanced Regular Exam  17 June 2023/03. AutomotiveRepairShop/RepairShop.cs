6b0c17ac3ec19cdd2e72760a4c6a7ae047946042
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-06 18:29:16 +0300

    Automotive Repair Shop

---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Regular Exam  17 June 2023/03. AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            this.Capacity = capacity;
+            this.Vehicles = new();
+        }
+
+        public int Capacity { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+        public int GetCount () { return this.Vehicles.Count; }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if(this.Vehicles.Count < this.Capacity)
+            {
+                this.Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            if(this.Vehicles.Any(x => x.VIN == vin))
+            {
+                Vehicle remulve = this.Vehicles.First(x => x.VIN == vin);
+                this.Vehicles.Remove(remulve);
+                return true;
+            }
+            return false;
+        }
+        public Vehicle GetLowestMileage()
+        {
+            return Vehicles.OrderBy(x =>x.Mileage).First();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("Vehicles in the preparatory:");
+            foreach (var vehicle in this.Vehicles)
+            {
+                sb.AppendLine(vehicle.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
