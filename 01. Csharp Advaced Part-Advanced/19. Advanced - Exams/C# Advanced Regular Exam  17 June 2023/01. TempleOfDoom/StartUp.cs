4e7e3b4844ad3631a94eb44d700a6261e6f510a4
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-06 14:01:08 +0300

    Temple of Doom

---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Regular Exam  17 June 2023/01. TempleOfDoom/StartUp.cs
@@ -0,0 +1,69 @@
+﻿namespace TempleOfDoom
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] toolsArraysInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int[] substancesArraysInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int[] challengeArraysInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Queue<int> tools = new(toolsArraysInput);
+            Stack<int> substances = new(substancesArraysInput);
+            List<int> challengers = new(challengeArraysInput);
+
+            while (true)
+            {
+                if (tools.Any() == false ||
+                    substances.Any() == false ||
+                    challengers.Any() == false)
+                {
+                    break;
+                }
+                int curentTool = tools.Dequeue();
+                int curentSubstrance = substances.Pop();
+                int curentResult = curentTool * curentSubstrance;
+
+                if (challengers.Any(x => x == curentResult))
+                {
+                    challengers.Remove(challengers.FirstOrDefault(x => x == curentResult));
+                    continue;
+                }
+
+                tools.Enqueue(++curentTool);
+
+                if (curentSubstrance - 1 > 0)
+                {
+                    substances.Push(--curentSubstrance);
+                }
+            }
+            if (challengers.Any())
+            {
+                Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+            }
+            else
+            {
+                Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+            }
+
+            if (tools.Any())
+            {
+                Console.WriteLine("Tools: " + string.Join(", ", tools));
+            }
+            if (substances.Any())
+            {
+                Console.WriteLine("Substances: " + string.Join(", ", substances));
+            }
+            if (challengers.Any())
+            {
+                Console.WriteLine("Challenges: " + string.Join(", ", challengers));
+            }
+        }
+    }
+}
