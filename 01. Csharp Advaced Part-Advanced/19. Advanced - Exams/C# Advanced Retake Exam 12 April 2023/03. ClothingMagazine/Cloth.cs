264e3a9bc421c61980f24f3f9be982223b432af0
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-10 02:06:22 +0300

    Clothes Magazine

---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Retake Exam 12 April 2023/03. ClothingMagazine/Cloth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesMagazine
+{
+    public class Cloth
+    {
+        public Cloth(string color, int size, string type)
+        {
+            Color = color;
+            Size = size;
+            Type = type;
+        }
+
+        public string Color { get; set; }
+        public int Size { get; set; }
+        public string Type { get; set; }
+
+        public override string ToString()
+        {
+            return $"Product: {this.Type} with size {this.Size}, color {this.Color}";
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Retake Exam 12 April 2023/03. ClothingMagazine/Magazine.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesMagazine
+{
+    public class Magazine
+    {
+        public Magazine(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            Clothes = new();
+        }
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public List<Cloth> Clothes { get; set; }
+
+        public int GetClothCount (){  return Clothes.Count; }
+
+        public void AddCloth(Cloth cloth)
+        {
+            if (this.Clothes.Count < this.Capacity)
+            {
+                Clothes.Add(cloth);
+            }
+        }
+
+        public bool RemoveCloth(string color)
+        {
+            if (Clothes.Any(x => x.Color == color))
+            {
+                Clothes.Remove(Clothes.FirstOrDefault(x => x.Color == color));
+                return true;
+            }
+            return false;
+        }
+        public Cloth GetSmallestCloth()
+        {
+            Cloth minValue = Clothes.MinBy(x => x.Size);
+            return minValue;
+        }
+        public Cloth GetCloth(string color)
+        {
+            return Clothes.FirstOrDefault(x => x.Color == color);
+        }
+        public string Report()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"{this.Type} magazine contains:");
+
+            foreach (var cloth in Clothes.OrderBy(x =>x.Size))
+            {
+                sb.AppendLine(cloth.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
