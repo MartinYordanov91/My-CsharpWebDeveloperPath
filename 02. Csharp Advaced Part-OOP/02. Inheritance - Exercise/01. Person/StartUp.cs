6d904068e95be8a0df5ec39d05493f80671d4bd0
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-12 20:38:43 +0300

    Adding

---
+++ 02. Csharp Advaced Part-OOP/02. Inheritance - Exercise/01. Person/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace InheritancePerson
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age > 15)
+            {
+                Person pesho = new(name, age);
+                Console.WriteLine(pesho);
+
+            }
+            else
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+        }
+    }
+}
---
+++ 02. Csharp Advaced Part-OOP/02. Inheritance - Exercise/05. PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            DarkKnight knight = new("Marto", 100);
+            Console.WriteLine(knight);
+        }
+    }
+}
