a0f4919f2db6ad5c729e06b8d957aac37f306974
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-09-14 21:51:27 +0300

    adding

---
+++ 02. Csharp Advaced Part-OOP/03. Encapsulation - Lab/02. Salary/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set { salary = value; }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal salaryIncrease = this.Salary * percentage / 100;
+            if(this.Age < 30)
+            {
+                this.Salary += salaryIncrease / 2;
+            }
+            else
+            {
+                this.salary += salaryIncrease ;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
