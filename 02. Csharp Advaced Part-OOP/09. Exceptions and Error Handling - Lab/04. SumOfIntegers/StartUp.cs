6ef5ac701ab2c357c3ae6d69a74dde56eb0480f1
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-10-10 20:12:54 +0300

    adding

---
+++ 02. Csharp Advaced Part-OOP/09. Exceptions and Error Handling - Lab/04. SumOfIntegers/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Linq;
+
+namespace SumOfIntegers
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string[] inputItems = Console.ReadLine().Split();
+            long sum = 0;
+
+            for (int i = 0; i < inputItems.Length; i++)
+            {
+                string item = inputItems[i];
+
+                bool isInteger = int.TryParse(item, out int intForSum);
+                bool isLonger = long.TryParse(item, out long longForResult);
+
+                if (isInteger)
+                {
+                    sum += intForSum;
+                    Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+                    continue;
+                }
+
+                try
+                {
+                    if (isLonger)
+                    {
+                        throw new OverflowException($"The element '{longForResult}' is out of range!");
+                    }
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+
+                try
+                {
+                    if (!isLonger)
+                    {
+                        throw new FormatException($"The element '{item}' is in wrong format!");
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine($"Element '{item}' processed - current sum: {sum}");
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
---
+++ 02. Csharp Advaced Part-OOP/09. Exceptions and Error Handling - Lab/05. PlayCatch/StartUp.cs
@@ -0,0 +1,98 @@
+﻿namespace PlayCatch
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int countExeptions = 0;
+            int[] intArray = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            while (countExeptions != 3)
+            {
+                string[] tokens = Console.ReadLine().Split();
+                string comand = tokens[0];
+
+                try
+                {
+                    if (comand == "Replace")
+                    {
+                        string index = tokens[1];
+                        string elemnt = tokens[2];
+                        int indexInt = FormatCorecly(index);
+                        int elementInt = FormatCorecly(elemnt);
+
+                        if (ArrayRange(intArray, indexInt))
+                        {
+                            intArray[indexInt] = elementInt;
+                        }
+
+                    }
+                    else if (comand == "Print")
+                    {
+                        string startIndex = tokens[1];
+                        string endIndex = tokens[2];
+                        int start = FormatCorecly(startIndex);
+                        int end = FormatCorecly(endIndex);
+
+                        if (ArrayRange(intArray, start) && ArrayRange(intArray, end))
+                        {
+                            Console.WriteLine(string.Join(", ", intArray.Range(start, end)));
+                        }
+                    }
+                    else if (comand == "Show")
+                    {
+                        string index = tokens[1];
+                        int indexElement = FormatCorecly(index);
+                        if(ArrayRange(intArray , indexElement))
+                        {
+                            Console.WriteLine(intArray[indexElement]);
+                        }
+                    }
+
+                }
+                catch (FormatException ex)
+                {
+                    countExeptions++;
+                    Console.WriteLine(ex.Message);
+                }
+                catch (OverflowException ex)
+                {
+                    countExeptions++;
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine(string.Join(", ", intArray));
+        }
+
+        public static int FormatCorecly(string numberMybe)
+        {
+            bool isNumber = int.TryParse(numberMybe, out int number);
+
+            if (!isNumber)
+            {
+                throw new FormatException("The variable is not in the correct format!");
+            }
+            return number;
+        }
+
+        public static bool ArrayRange(int[] array, int index)
+        {
+            if (index < 0 || array.Length <= index)
+            {
+                throw new OverflowException("The index does not exist!");
+            }
+            return true;
+        }
+    }
+    public static class Extension
+    {
+        public static int[] Range(this int[] array, int start, int end)
+        {
+            List<int> list = array.ToList();
+            return list.GetRange(start, end - start+1).ToArray();
+        }
+    }
+}
