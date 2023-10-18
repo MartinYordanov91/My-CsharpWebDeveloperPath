980e84f27f6c0595fd03418c9c9d08d81452918e
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2023-10-19 00:15:40 +0300

    adding

---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Exam 25 June 2022/01. TilesMaster/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TilesMaster
+{
+    public class StartUp
+    {
+
+        public static void Main(string[] args)
+        {
+            string sink = "Sink";
+            string oven = "Oven";
+            string countertop = "Countertop";
+            string wall = "Wall";
+            string floor = "Floor";
+
+            Dictionary<string, int> colection = new Dictionary<string, int>();
+            Stack<int> white = new Stack<int>();
+            Queue<int> grey = new Queue<int>();
+
+            int[] sequenceWhite = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+
+            int[] sequenceGrey = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+
+            foreach (int w in sequenceWhite)
+            {
+                white.Push(w);
+            }
+
+            foreach (int g in sequenceGrey)
+            {
+                grey.Enqueue(g);
+            }
+
+            while (white.Any() && grey.Any())
+            {
+                int curentWhite = white.Pop();
+                int curentGrey = grey.Dequeue();
+
+                if (curentWhite != curentGrey)
+                {
+                    white.Push(curentWhite / 2);
+                    grey.Enqueue(curentGrey);
+
+                    continue;
+                }
+
+                int sum = curentGrey + curentWhite;
+                if (sum == 40) { ImplementObj(colection, sink); }
+                else if (sum == 50) { ImplementObj(colection, oven); }
+                else if (sum == 60) { ImplementObj(colection, countertop); }
+                else if (sum == 70) { ImplementObj(colection, wall); }
+                else { ImplementObj(colection, floor); }
+            }
+
+
+
+            Console.WriteLine(white.Any()
+                ? $"White tiles left: {string.Join(", ", white)}"
+                : "White tiles left: none");
+
+            Console.WriteLine(grey.Any()
+                    ? $"Grey tiles left: {string.Join(", ", grey)}"
+                    : "Grey tiles left: none");
+            // Variant 1
+            //foreach (var item in colection.OrderByDescending(x => x.Value).ThenBy(v => v.Key))
+            //{
+            //    Console.WriteLine($"{item.Key}: {item.Value}");
+            //}
+
+            //variant 2
+            Console.WriteLine(string.Join(Environment.NewLine , colection.OrderByDescending(x => x.Value).ThenBy(v => v.Key).Select(k => k.Key +": "+ k.Value)));
+        }
+        public static void ImplementObj(Dictionary<string, int> colection, string item)
+        {
+            if (!colection.ContainsKey(item))
+            {
+                colection[item] = 0;
+            }
+
+            colection[item]++;
+        }
+    }
+}
---
+++ 01. Csharp Advaced Part-Advanced/19. Advanced - Exams/C# Advanced Exam 25 June 2022/02. WallDestroyer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace WallDestroyer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] fild = new char[size, size];
+
+            int vankoRow = 0, vankoCol = 0;
+            int rodCount = 0;
+            int holeCount = 1;
+            bool isElektrocute = false;
+
+            for (int row = 0; row < fild.GetLength(0); row++)
+            {
+                string curentCol = Console.ReadLine();
+
+                for (int col = 0; col < fild.GetLength(1); col++)
+                {
+                    fild[row, col] = curentCol[col];
+
+                    if (fild[row, col] == 'V')
+                    {
+                        vankoRow = row;
+                        vankoCol = col;
+
+                    }
+                }
+            }
+
+            string comand = string.Empty;
+
+            while ((comand = Console.ReadLine()) != "End")
+            {
+                int copyRow = vankoRow;
+                int copyCol = vankoCol;
+
+                switch (comand)
+                {
+                    case "up": vankoRow--; break;
+                    case "down": vankoRow++; break;
+                    case "left": vankoCol--; break;
+                    case "right": vankoCol++; break;
+                }
+
+                if (vankoRow < 0 || vankoCol < 0
+                    || vankoRow >= fild.GetLength(0)
+                    || vankoCol >= fild.GetLength(1))
+                {
+                    vankoRow = copyRow;
+                    vankoCol = copyCol;
+                    continue;
+                }
+
+                if (fild[vankoRow,vankoCol] == 'R')
+                {
+                    rodCount++;
+                    vankoRow = copyRow;
+                    vankoCol = copyCol;
+                    Console.WriteLine("Vanko hit a rod!");
+                    continue;
+                }
+                
+                fild[copyRow, copyCol] = '*';
+
+                if(fild[vankoRow, vankoCol] == '*')
+                {
+                    Console.WriteLine($"The wall is already destroyed at position [{vankoRow}, {vankoCol}]!");
+                    continue;
+                }
+
+                holeCount++;
+
+                if(fild[vankoRow, vankoCol] == 'C')
+                {
+                    Console.WriteLine($"Vanko got electrocuted, but he managed to make {holeCount} hole(s).");
+                    fild[vankoRow, vankoCol] = 'E';
+                    isElektrocute = true;
+                    break;
+                }
+                fild[vankoRow, vankoCol] = 'V';
+            }
+
+            if (fild[vankoRow, vankoCol] != 'E') { fild[vankoRow, vankoCol] = 'V'; }
+
+            if (!isElektrocute)
+            {
+                Console.WriteLine($"Vanko managed to make {holeCount} hole(s) and he hit only {rodCount} rod(s).");
+            }
+
+            for (int r = 0; r < fild.GetLength(0); r++)
+            {
+                for (int c = 0; c < fild.GetLength(1); c++)
+                {
+                    Console.Write(fild[r,c]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
