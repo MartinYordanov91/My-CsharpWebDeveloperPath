79c3ed80a2d3aa1ebf93d63e1be27594bbc82215
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-03-06 19:14:43 +0200

    dd

--- 04. JS Front-End/08. Functions and Statements - Exercises/05. Palindrome Integers/index.js
+++ 04. JS Front-End/08. Functions and Statements - Exercises/05. Palindrome Integers/index.js
@@ -1,11 +1,11 @@
 function isPalindrome(params) {
-    for (const number of params) {
-        let string =number.toString()
-        let reversedString = number.toString().split('').reverse().join('')
-        console.log(string === reversedString)
-    }
+  for (const number of params) {
+    let string = number.toString();
+    let reversedString = number.toString().split("").reverse().join("");
+
+    console.log(string === reversedString);
+  }
 }
 
-
-isPalindrome([123,323,421,121])
-isPalindrome([32,2,232,1010])
+isPalindrome([123, 323, 421, 121]);
+isPalindrome([32, 2, 232, 1010]);
