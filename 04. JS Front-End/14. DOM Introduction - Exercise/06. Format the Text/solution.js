9a6b6c776ec9764c17099133884d31cab9b85b80
MartinYordanov91 <124835928+MartinYordanov91@users.noreply.github.com>
2024-03-26 21:27:22 +0200

    add

--- 04. JS Front-End/14. DOM Introduction - Exercise/06. Format the Text/solution.js
+++ 04. JS Front-End/14. DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,19 @@
 function solve() {
-  //TODO
+  const inputAreaElement = document.getElementById("input");
+  const outputAreaElement = document.getElementById("output");
+
+  let sentencesArray = inputAreaElement.value
+    .split(".")
+    .filter((sentence) => sentence.trim() !== "")
+    .map((sentence) => sentence.trim() + ".");
+
+  if (sentencesArray.length <= 3) {
+    outputAreaElement.innerHTML = `<p>${sentencesArray.join(" ")}</p>`;
+  } else {
+    let paragraphs = [];
+    for (let i = 0; i < sentencesArray.length; i += 3) {
+      paragraphs.push(`<p>${sentencesArray.slice(i, i + 3).join(" ")}</p>`);
+    }
+    outputAreaElement.innerHTML = paragraphs.join("");
+  }
 }
---
+++ 04. JS Front-End/test.js
@@ -0,0 +1,5 @@
+let desimalNumber = 10;
+let binaryNumber = desimalNumber.toString(2);
+let hexaDesimalNumber = desimalNumber.toString(16).toUpperCase();
+
+console.log(binaryNumber);
